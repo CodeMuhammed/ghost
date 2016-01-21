66159deb7997571cd3fc8a2a6d8794ddb5542ba4
codemuhammed <codemuhammed@gmail.com>
2016-01-21 02:29:34 -0800

    done

--- web.js
+++ web.js
@@ -8,67 +8,79 @@ var Spooky = require('spooky');
 // [Getting Started]: https://devcenter.heroku.com/articles/getting-started-with-nodejs
 // [Spooky]: https://github.com/WaterfallEngineering/SpookyJS
 
-var spooky = new Spooky({
-        child: {
-            transport: 'http'
-        },
-        casper: {
-            logLevel: 'debug',
-            verbose: true
-        }
-    }, function (err) {
-        if (err) {
-            e = new Error('Failed to initialize SpookyJS');
-            e.details = err;
-            throw e;
-        }
 
-        spooky.start(
-            'http://www.palingram.com/ads-test.html');
-        spooky.then(function () {
-            this.emit('hello', 'Hello, from ' + this.evaluate(function () {
-                return document.title;
-            }));
-        });
-        spooky.run();
-    });
-
-spooky.on('error', function (e, stack) {
-    console.error(e);
-
-    if (stack) {
-        console.log(stack);
-    }
-});
-
-/*
-// Uncomment this block to see all of the things Casper has to say.
-// There are a lot.
-// He has opinions.
-spooky.on('console', function (line) {
-    console.log(line);
-});
-*/
+//**************** GHOST MODE***************************
+var counter = 0;
 var gGreeting = 'Hello World';
 
-spooky.on('hello', function (greeting) {
-    console.log(greeting);
-    gGreeting = greeting;
-});
+function creep() {
+	var spooky = new Spooky({
+	   child: {
+				transport: 'http'
+			},
+			casper: {
+				logLevel: 'debug',
+				verbose: true
+			}
+		}, function (err) {
+			if (err) {
+				e = new Error('Failed to initialize SpookyJS');
+				e.details = err;
+				throw e;
+			}
 
-spooky.on('log', function (log) {
-    if (log.space === 'remote') {
-        console.log(log.message.replace(/ \- .*/, ''));
-    }
-});
+			spooky.start(
+				'http://www.palingram.com/ads-test.html');
+			spooky.then(function () {
+				this.emit('hello', 'Hello, from ' + this.evaluate(function () {
+					return document.title;
+				}));
+			});
+			spooky.run();
+		});
+
+	spooky.on('error', function (e, stack) {
+		console.error(e);
+
+		if (stack) {
+			console.log(stack);
+		}
+	});
+
+	/*
+	// Uncomment this block to see all of the things Casper has to say.
+	// There are a lot.
+	// He has opinions.
+	spooky.on('console', function (line) {
+		console.log(line);
+	});
+	*/
+
+
+	spooky.on('hello', function (greeting) {
+		console.log(greeting);
+		gGreeting = greeting;
+		counter++;
+		creep();
+	});
+
+	spooky.on('log', function (log) {
+		if (log.space === 'remote') {
+			console.log(log.message.replace(/ \- .*/, ''));
+		}
+	});
+}
+creep();
+//******************************************************
+
 
 
 app.use(express.logger());
 app.get('/', function(request, response) {
-    response.send(gGreeting);
+    response.send(gGreeting+" "+counter+" times");
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3001;
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
