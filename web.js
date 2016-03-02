feb10edeb4771884197bb6348346602fcc0b72a5
codemuhammed <codemuhammed@gmail.com>
2016-03-02 04:50:43 -0800

    Signed-off-by: codemuhammed <codemuhammed@gmail.com>

--- web.js
+++ web.js
@@ -5,15 +5,80 @@ var express = require("express");
 var app = express();
 var path = require('path');
 var Spooky = require('spooky');
-
-
+var LineByLineReader = require('line-by-line');
 
 //run main non ip address changing bot
 var gGreeting = 'Hello World';
 var counter = 0;
-console.log(gGreeting);
+var ghostIpUrls = [];
+var urls = [
+   'http://www.palingram.com/ads-test.html',
+   'http://fg20.herokuapp.com',
+   'http://fg20.herokuapp.com',
+   'http://fg20.herokuapp.com',
+   'http://fg20.herokuapp.com',
+   'http://fg20.herokuapp.com',
+   'http://fg20.herokuapp.com',
+   'http://fg20.herokuapp.com',
+   'http://fg20.herokuapp.com',
+   'http://fg20.herokuapp.com',
+   'http://www.linkbucks.com/fk8Y',
+];
+var runGhostWhite;
 
-var spooky = new Spooky({
+//
+function pingGhostIp(cb){
+	//
+    console.log('heroku ping here');
+    //Read lines of ip use them to make request before resulting to gimmeproxy
+	ha = new LineByLineReader('ghostipapps.txt');
+	ha.on('error', function (err) {
+		console.log('error while reading file');
+	});
+
+	ha.on('line', function (line) {
+		ghostIpUrls.push(line.toString());
+	});
+
+	ha.on('end', function () {
+	   console.log(ghostIpUrls.length);
+	   
+	   //do pinging
+	   var currentUrl = 0;
+	   function doPing(){
+	       if(currentUrl < ghostIpUrls.length){
+		        request.get(ghostIpUrls[currentUrl] , function(err , response , body){
+					 if(err){
+						 console.log(err);
+						 currentUrl++;					 
+						 doPing();
+					 } 
+					 else {
+					     Greeting = ghostIpUrls[currentUrl]+' test done';
+						 console.log(ghostIpUrls[currentUrl]+' test done'); 
+						 currentUrl++;					 
+						 doPing();
+					 }
+				 });
+		   }
+		   else{
+			  cb();
+		      return;
+		   }  
+	   };
+	   
+	   doPing();
+	});
+};
+//initial pinging of ghostip
+pingGhostIp(function(){
+	console.log('Initial pinging done');
+	Greeting = 'Initial pinging done';
+	initSpooky();
+});
+
+function initSpooky(){
+	var spooky = new Spooky({
         child: {
             transport: 'http'
         },
@@ -27,65 +92,44 @@ var spooky = new Spooky({
             e.details = err;
             throw e;
         }
-		console.log('here here');
-        spooky.start('http://www.palingram.com/ads-test.html');
 		
-		spooky.then(function () {
-			this.urls = [
-			   'http://www.upbunk.com/ads-test.html',
-			   'http://www.palingram.com/ads-test.html',
-			   'http://fg20.herokuapp.com',
-			   'http://fg20.herokuapp.com',
-			   'http://fg20.herokuapp.com',
-			   'http://fg20.herokuapp.com',
-			   'http://fg20.herokuapp.com',
-			   'http://fg20.herokuapp.com',
-			   'http://fg20.herokuapp.com',
-			   'http://fg20.herokuapp.com',
-			   'http://fg20.herokuapp.com',
-			   'http://www.linkbucks.com/fk8Y',
-			];
-			
-			this.counter = 0;
-            this.urlSize = this.urls.length;
-			this.hhh = function(url){
-				this.start(url);
-				this.then(function () {
-				   this.emit('hello', 'Hello, from ' + this.evaluate(function () {
+		runGhostWhite = function(){
+			spooky.start(urls[counter%urls.length]);
+			spooky.then(function(){
+				this.wait(30000 , function(){
+					this.emit('hello' , this.evaluate(function () {
 						return document.title;
 				   }));
-				   phantom.clearCookies();
-				   this.counter++;
-				   this.wait(10 , function(){
-					   this.hhh(this.urls[this.counter%this.urlSize]);
-				   });
 				});
-			};
-			this.hhh(this.urls[this.counter%this.urlSize]); 
-		});
+			});
+		}
+        runGhostWhite();
 		
         spooky.run();
     });
-
+	
 	spooky.on('error', function (e, stack) {
-		console.error(e);
-
+		console.error('this '+e);
 		if (stack) {
-			console.log(stack);
+			console.log('this here '+stack);
 		}
+		counter++;
+		gGreeting = e;
+		runGhostWhite();
 	});
 
 	spooky.on('hello', function (greeting) {
 		console.log(greeting);
 		counter++;
 		gGreeting = greeting;
+		runGhostWhite();
 	});
+	
+	spooky.on('console', function (line) {
+		console.log(line);
+	});
+}
 
-	spooky.on('log', function (log) {
-		if (log.space === 'remote') {
-			console.log(log.message.replace(/ \- .*/, ''));
-		}
-	});
 
 //
 app.use(express.static(path.join(__dirname , 'public')));
@@ -96,7 +140,10 @@ app.get('/stats', function(request, response) {
 
 //restarts the app after every 500 visits and 20 minutes of app's uptime
 setTimeout(function(){
-	process.exit(0); 
+	//initial pinging of ghostip
+	pingGhostIp(function(){
+		process.exit(0);
+	});
 } , 60000*60);
 
 var port = process.env.PORT || 5000;
